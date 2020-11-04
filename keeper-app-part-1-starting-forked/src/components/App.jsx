6ee42ad26670078aafc6e9c567077a8f28f429ec
Nguyen Huy Long <45191369+jc487497@users.noreply.github.com>
2020-11-04 09:49:21 +0700

    Add files via upload

---
+++ keeper-app-part-1-starting-forked/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note.jsx";
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
